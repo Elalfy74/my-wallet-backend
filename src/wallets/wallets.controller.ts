f3a5779dc0b6ffd6a28ed462e334acd1a0a5c8d7
elalfy74 <mahmoudelalfy13@gmail.com>
2023-06-23 17:32:02 +0300

    added wallet get route

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,17 +1,35 @@
-import { Controller, Post, Session, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { ISession } from 'src/auth/interfaces';
 import { AuthGuard } from 'src/guards';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateWalletDto, WalletDto } from './dtos';
+import { Serialize } from 'src/interceptors';
 
 @Controller('wallets')
 @UseGuards(AuthGuard)
+@Serialize(WalletDto)
 @ApiTags('Wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
   @Post()
-  async create(@Session() session: ISession) {
-    await this.walletsService.create(session);
+  async create(
+    @Body() createWalletDto: CreateWalletDto,
+    @Session() session: ISession,
+  ) {
+    await this.walletsService.create(createWalletDto, session);
     return 'Wallet Created Successfully';
+  }
+
+  @Get()
+  findOne(@Session() session: ISession) {
+    return this.walletsService.findOne(session);
   }
 }
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,26 +1,36 @@
 import {
   ForbiddenException,
   Injectable,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import {
+  Decimal,
+  PrismaClientKnownRequestError,
+} from '@prisma/client/runtime/library';
 import { ISession } from 'src/auth/interfaces';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateWalletDto } from './dtos';
 
 @Injectable()
 export class WalletsService {
   constructor(private prisma: PrismaService) {}
-  async create(session: ISession) {
+  async create(createWalletDto: CreateWalletDto, session: ISession) {
     try {
       return await this.prisma.wallet.create({
         data: {
           userId: session.userId,
+          name: createWalletDto.name,
         },
       });
     } catch (e) {
       if (e instanceof PrismaClientKnownRequestError) {
         if (e.code === 'P2002') {
-          throw new ForbiddenException('User Already have a wallet');
+          const msg =
+            e.meta?.target === 'name'
+              ? 'Name already used'
+              : 'User Already has a wallet';
+          throw new ForbiddenException(msg);
         }
         if (e.code === 'P2003') {
           throw new UnauthorizedException();
@@ -28,5 +38,18 @@ export class WalletsService {
       }
       throw e;
     }
+  }
+
+  async findOne(session: ISession) {
+    const wallet = await this.prisma.wallet.findUnique({
+      where: {
+        userId: session.userId,
+      },
+    });
+    const formattedBalance = new Decimal(wallet.balance.toString()).toNumber();
+
+    if (!wallet) throw new NotFoundException('No Wallet found');
+
+    return { ...wallet, balance: formattedBalance };
   }
 }
